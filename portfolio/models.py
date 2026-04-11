3123dce72a4c63a896527c1de292e98f24f814c1
a22409245DiogoSa <a22409245@alunos.ulht.pt>
2026-04-11 16:11:32 +0000

    Adicionados registos de Making Of com imagens

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,11 +3,15 @@ from django.db import models
 class Licenciatura(models.Model):
     nome = models.CharField(max_length=100)
     apresentacao = models.TextField()
+    def __str__(self):
+        return self.nome
 
 class Tecnologia(models.Model):
     nome = models.CharField(max_length=50)
     logotipo = models.ImageField(upload_to='tecnologias/', blank=True)
     link_oficial = models.URLField()
+    def __str__(self):
+        return self.nome
 
 class UnidadeCurricular(models.Model):
     nome = models.CharField(max_length=100)
@@ -15,6 +19,8 @@ class UnidadeCurricular(models.Model):
     ects = models.IntegerField()
     docente = models.CharField(max_length=100)
     licenciatura = models.ForeignKey(Licenciatura, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nome
 
 class Projeto(models.Model):
     titulo = models.CharField(max_length=100)
@@ -23,8 +29,12 @@ class Projeto(models.Model):
     github = models.URLField()
     uc = models.ForeignKey(UnidadeCurricular, on_delete=models.CASCADE)
     tecnologias = models.ManyToManyField(Tecnologia)
+    def __str__(self):
+        return self.titulo
 
 class MakingOf(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField()
     foto_caderno = models.ImageField(upload_to='makingof/')
+    def __str__(self):
+        return self.titulo
